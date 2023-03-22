8428f198755797102dea1812fc3867aec3c75cda
ChiJastir <sanya.chizh.00@bk.ru>
2023-03-23 02:22:19 +0300

    diversified posts

--- src/components/profile/posts/post/post.jsx
+++ src/components/profile/posts/post/post.jsx
@@ -1,10 +1,10 @@
 import s from './post.module.css'
 
-function Post(){
+function Post(props){
     return(
         <div className={s.post}>
-            <img className={s.avatar} src="https://coolsen.ru/wp-content/uploads/2021/09/image143-6.jpg" alt="avatar"/>
-            <p className={s.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, consequatur, fugiat? Beatae id ipsam iste magnam molestiae mollitia, nobis placeat reiciendis repellendus vitae. Delectus deleniti illo quaerat, reprehenderit sed tenetur!</p>
+            <img className={s.avatar} src={props.avatar} alt="avatar"/>
+            <p className={s.text}>{props.content}</p>
         </div>
     )
 }
--- src/components/profile/posts/posts.jsx
+++ src/components/profile/posts/posts.jsx
@@ -7,10 +7,10 @@ function Posts(){
         <div>
             <h3 className={s.title}>My posts</h3>
             <AddPost/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
+            <Post avatar={"https://coolsen.ru/wp-content/uploads/2021/09/image143-6.jpg"} content={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aut consectetur consequuntur dolorem eius eum explicabo, iusto mollitia nihil nisi, ratione saepe sapiente totam veritatis vitae, voluptate voluptatum. Quam, reiciendis."}/>
+            <Post avatar={"https://kartinkin.net/uploads/posts/2021-07/1625518254_42-kartinkin-com-p-top-avi-anime-anime-krasivo-42.jpg"} content={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias amet architecto aspernatur beatae corporis deleniti esse fugit, harum molestiae mollitia nihil nulla odio, placeat quae quasi repellat tempora velit."}/>
+            <Post avatar={"https://i.pinimg.com/originals/18/c7/2c/18c72c635bcd4bddad6c6f27ab365aad.jpg"} content={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo impedit incidunt maiores molestiae ratione! Beatae dignissimos, et explicabo facilis fuga id, iste magni maiores minima neque repudiandae ut veritatis, voluptatum."}/>
+            <Post avatar={"https://uprostim.com/wp-content/uploads/2021/02/image190-33.jpg"} content={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. At commodi dolore dolorem doloremque dolores earum eligendi explicabo impedit ipsa labore minima nam, nobis numquam pariatur perspiciatis qui sequi tempora. Doloremque!"}/>
         </div>
     )
 }
